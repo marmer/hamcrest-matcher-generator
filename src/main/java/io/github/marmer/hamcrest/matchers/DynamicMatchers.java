6fc8049dc5cc3efa1551b99125e341d6d059c18c
MarMer <Mariano.Mertinat@gmail.com>
2017-06-12 21:54:01 +0200

    little refactoring

--- src/main/java/io/github/marmer/hamcrest/matchers/DynamicMatchers.java
+++ src/main/java/io/github/marmer/hamcrest/matchers/DynamicMatchers.java
@@ -1,38 +1,25 @@
 package io.github.marmer.hamcrest.matchers;
 
-import static org.hamcrest.Matchers.hasProperty;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
-import org.hamcrest.TypeSafeMatcher;
-
+/**
+ * Collection of dynamic Matchers.
+ *
+ * @author marmer
+ * @date 12.06.2017
+ *
+ */
 public class DynamicMatchers {
 
-	public static final class DynamicPropertyMatcher<T> extends TypeSafeMatcher<T> {
-
-		private Matcher<T> instanceOfMatcher;
-
-		private DynamicPropertyMatcher(final Class<T> expectedClass) {
-			this.instanceOfMatcher = Matchers.instanceOf(expectedClass);
-		}
-
-		public Matcher<T> withMyFancyProperty() {
-			// FIXME static implementation is only a helper to prepare a test.
-			// FIXME Make me dynamic
-			return hasProperty("myFancyProperty");
-		}
-
-		public void describeTo(final Description description) {
-			instanceOfMatcher.describeTo(description);
-		}
-
-		@Override
-		protected boolean matchesSafely(final T item) {
-			return instanceOfMatcher.matches(item);
-		}
-	}
-
+	/**
+	 * Prepares a DynamicPropertyMatcher for the given Instance.
+	 *
+	 * TODO find a better name.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param type
+	 *            the type
+	 * @return the dynamic property matcher
+	 */
 	public static <T> DynamicPropertyMatcher<T> instanceOf(final Class<T> type) {
 		return new DynamicPropertyMatcher<T>(type);
 	}
