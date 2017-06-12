0c328c23142e31cd353cd0185f977ee146e9cb5e
MarMer <1055445+marmer@users.noreply.github.com>
2017-06-12 21:54:01 +0200

    little refactoring

---
+++ src/main/java/io/github/marmer/hamcrest/matchers/DynamicPropertyMatcher.java
@@ -0,0 +1,32 @@
+package io.github.marmer.hamcrest.matchers;
+
+import static org.hamcrest.Matchers.hasProperty;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+
+public final class DynamicPropertyMatcher<T> extends TypeSafeMatcher<T> {
+
+	private Matcher<T> instanceOfMatcher;
+
+	DynamicPropertyMatcher(final Class<T> expectedClass) {
+		this.instanceOfMatcher = Matchers.instanceOf(expectedClass);
+	}
+
+	public Matcher<T> withMyFancyProperty() {
+		// FIXME static implementation is only a helper to prepare a test.
+		// FIXME Make me dynamic
+		return hasProperty("myFancyProperty");
+	}
+
+	public void describeTo(final Description description) {
+		instanceOfMatcher.describeTo(description);
+	}
+
+	@Override
+	protected boolean matchesSafely(final T item) {
+		return instanceOfMatcher.matches(item);
+	}
+}
