9d3e9711e8507b150c95cd984571e7675a046ae3
MarMer <Mariano.Mertinat@gmail.com>
2017-06-15 20:24:02 +0200

    another little cleanup

--- src/main/java/io/github/marmer/testutils/samplepojos/HasPropertyMatcherGenerator.java
+++ src/main/java/io/github/marmer/testutils/samplepojos/HasPropertyMatcherGenerator.java
@@ -5,10 +5,7 @@ import java.nio.file.Path;
 
 import javax.lang.model.element.Modifier;
 
-import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
 public class HasPropertyMatcherGenerator {
@@ -16,13 +13,17 @@ public class HasPropertyMatcherGenerator {
 	private static final String POSTFIX = "Matcher";
 
 	public void generateMatcherFor(final Class<?> type, final Path outputDir) throws IOException {
-		CodeBlock codeBlock = CodeBlock.builder().add("System.out.println(\"mops\");").build();
-		MethodSpec isType = MethodSpec.methodBuilder("isSimplePojo").returns(TypeName.VOID).addCode(codeBlock).build();
-		TypeSpec typeSpec = TypeSpec.classBuilder(type.getSimpleName() + POSTFIX).addModifiers(Modifier.PUBLIC)
-				.addMethod(isType).build();
-		JavaFile javaFile = JavaFile.builder(type.getPackage().getName(), typeSpec).indent("\t")
-				.skipJavaLangImports(true).build();
+		JavaFile javaFile = prepareType(type);
 		javaFile.writeTo(outputDir);
+	}
+
+	private JavaFile prepareType(final Class<?> type) {
+		TypeSpec typeSpec = prepareClass(type);
+		return JavaFile.builder(type.getPackage().getName(), typeSpec).indent("\t").skipJavaLangImports(true).build();
+	}
+
+	private TypeSpec prepareClass(final Class<?> type) {
+		return TypeSpec.classBuilder(type.getSimpleName() + POSTFIX).addModifiers(Modifier.PUBLIC).build();
 	}
 
 }
