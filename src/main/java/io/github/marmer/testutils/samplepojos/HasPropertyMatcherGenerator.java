a3c6b78055b458353401e46e8da03b97dea34314
MarMer <1055445+marmer@users.noreply.github.com>
2017-06-15 22:50:44 +0200

    generated class can be used to match types now

--- src/main/java/io/github/marmer/testutils/samplepojos/HasPropertyMatcherGenerator.java
+++ src/main/java/io/github/marmer/testutils/samplepojos/HasPropertyMatcherGenerator.java
@@ -5,7 +5,10 @@ import java.nio.file.Path;
 
 import javax.lang.model.element.Modifier;
 
+import org.hamcrest.core.IsInstanceOf;
+
 import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 
 public class HasPropertyMatcherGenerator {
@@ -18,12 +21,18 @@ public class HasPropertyMatcherGenerator {
 	}
 
 	private JavaFile prepareType(final Class<?> type) {
-		TypeSpec typeSpec = prepareClass(type);
+		TypeSpec typeSpec = type(type);
 		return JavaFile.builder(type.getPackage().getName(), typeSpec).indent("\t").skipJavaLangImports(true).build();
 	}
 
-	private TypeSpec prepareClass(final Class<?> type) {
-		return TypeSpec.classBuilder(type.getSimpleName() + POSTFIX).addModifiers(Modifier.PUBLIC).build();
+	private TypeSpec type(final Class<?> type) {
+		return TypeSpec.classBuilder(type.getSimpleName() + POSTFIX).superclass(IsInstanceOf.class)
+				.addModifiers(Modifier.PUBLIC).addMethod(constructor(type)).build();
+	}
+
+	private MethodSpec constructor(final Class<?> type) {
+		return MethodSpec.constructorBuilder().addStatement("super($T.class)", type).addModifiers(Modifier.PUBLIC)
+				.build();
 	}
 
 }
