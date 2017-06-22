f6ba3c726272e9fead932e544ff088ccfd16a636
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2017-06-22 12:43:45 +0200

    a bit dependency management and test whether it's possible to execute the mojo and to run tests after without any errors added

--- java-test-helper-generator-maven-plugin/src/main/java/org/java/test/helper/generator/maven/plugin/MatchersMojo.java
+++ java-test-helper-generator-maven-plugin/src/main/java/org/java/test/helper/generator/maven/plugin/MatchersMojo.java
@@ -3,13 +3,16 @@ package org.java.test.helper.generator.maven.plugin;
 /*
  * Copyright 2001-2005 The Apache Software Foundation.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the
  * License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the
  * specific language governing permissions and limitations under the License.
  */
 
@@ -27,49 +30,45 @@ import java.io.IOException;
 /**
  * Goal which touches a timestamp file.
  *
- * @author  marmer
- * @date    21.06.2017
+ * @author marmer
+ * @date   21.06.2017
  */
-@Mojo(
-	name = "matchers",
-	defaultPhase = LifecyclePhase.PROCESS_SOURCES,
-	threadSafe = false
-)
+@Mojo(name = "matchers", defaultPhase = LifecyclePhase.PROCESS_SOURCES, threadSafe = false)
 public class MatchersMojo extends AbstractMojo {
+    /** Location of the file. */
+    @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
+    private File outputDirectory;
 
-	/** Location of the file. */
-	@Parameter(
-		defaultValue = "${project.build.directory}",
-		property = "outputDir",
-		required = true
-	)
-	private File outputDirectory;
+    @Override
+    public void execute() throws MojoExecutionException {
+        if (true) {
+            throw new MojoExecutionException("Life is fun");
+        }
 
-	@Override
-	public void execute() throws MojoExecutionException {
-		final File f = outputDirectory;
+        final File f = outputDirectory;
 
-		if (!f.exists()) {
-			f.mkdirs();
-		}
+        if (!f.exists()) {
+            f.mkdirs();
+        }
 
-		final File touch = new File(f, "touch.txt");
+        final File touch = new File(f, "touch.txt");
 
-		FileWriter w = null;
-		try {
-			w = new FileWriter(touch);
+        FileWriter w = null;
 
-			w.write("touch.txt");
-		} catch (IOException e) {
-			throw new MojoExecutionException("Error creating file " + touch, e);
-		} finally {
-			if (w != null) {
-				try {
-					w.close();
-				} catch (IOException e) {
-					// ignore
-				}
-			}
-		}
-	}
+        try {
+            w = new FileWriter(touch);
+
+            w.write("touch.txt");
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        } finally {
+            if (w != null) {
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+    }
 }
