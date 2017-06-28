288d19a905e4f3376c31c703118672b12e35c4e1
MarMer <1055445+marmer@users.noreply.github.com>
2017-06-28 11:54:24 +0200

    - lombok removed (temporarily) - needed hamcrest dependency added within sample testproject - testproject-test changed to use one of the generated matchers to see the build failing until needed dependent classes are not missing anymore

--- java-test-helper-generator-maven-plugin-systemtests/src/test/projects/testproject/src/test/java/some/pck/service/impl/SimpleInterfaceImplTest.java
+++ java-test-helper-generator-maven-plugin-systemtests/src/test/projects/testproject/src/test/java/some/pck/service/impl/SimpleInterfaceImplTest.java
@@ -2,11 +2,20 @@ package some.pck.service.impl;
 
 import org.junit.Test;
 
+import some.pck.model.SimpleModel;
+import some.pck.model.SimpleModelMatcher;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import static org.junit.Assert.assertThat;
+
 
 public class SimpleInterfaceImplTest {
 	@Test
 	public void testSomeTestWhichDoingSomething() throws Exception {
-		System.out.println("Doing something");
+		assertThat(new SimpleModel("someValue"), is(new SimpleModelMatcher().withSomeProperty(equalTo("someValue"))));
 	}
 
 }
