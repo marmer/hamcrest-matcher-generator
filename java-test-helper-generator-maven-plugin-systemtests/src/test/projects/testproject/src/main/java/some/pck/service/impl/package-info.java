fab838a4b8d1b1776e555b0fd988dcd9859c4558
MarMer <1055445+marmer@users.noreply.github.com>
2017-06-28 21:21:59 +0200

    System tests for the maven plugin are back again within the maven pluigin module itself

--- java-test-helper-generator-maven-plugin-systemtests/src/test/projects/testproject/src/main/java/some/pck/service/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Service implementations.
- *
- * @author  marmer
- * @date    21.06.2017
- */
-package some.pck.service.impl;
--- java-test-helper-generator-maven-plugin-systemtests/src/test/projects/testproject/src/main/java/some/pck/service/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Services.
- *
- * @author  marmer
- * @date    21.06.2017
- */
-package some.pck.service;
