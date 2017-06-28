6e6751df71a981d6021f9197d70ec68ed9bac1c8
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2017-06-28 11:54:24 +0200

    - lombok removed (temporarily) - needed hamcrest dependency added within sample testproject - testproject-test changed to use one of the generated matchers to see the build failing until needed dependent classes are not missing anymore

--- java-test-helper-generator-maven-plugin-systemtests/src/test/projects/testproject/src/main/java/some/pck/model/SimpleModel.java
+++ java-test-helper-generator-maven-plugin-systemtests/src/test/projects/testproject/src/main/java/some/pck/model/SimpleModel.java
@@ -1,9 +1,48 @@
 package some.pck.model;
 
-import lombok.Value;
-
-
-@Value
 public class SimpleModel {
-	private String someProperty;
+	private final String someProperty;
+
+	public SimpleModel(final String someProperty) {
+		this.someProperty = someProperty;
+	}
+
+	@Override
+	public String toString() {
+		return "SimpleModel [someProperty=" + someProperty + "]";
+	}
+
+	public String getSomeProperty() {
+		return someProperty;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result) + ((someProperty == null) ? 0 : someProperty.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof SimpleModel)) {
+			return false;
+		}
+		final SimpleModel other = (SimpleModel) obj;
+		if (someProperty == null) {
+			if (other.someProperty != null) {
+				return false;
+			}
+		} else if (!someProperty.equals(other.someProperty)) {
+			return false;
+		}
+		return true;
+	}
 }
