6e13b5935dd476b1e111d3a8cf1030865ff7f659
MarMer <Mariano.Mertinat@gmail.com>
2017-06-28 21:21:59 +0200

    System tests for the maven plugin are back again within the maven pluigin module itself

--- java-test-helper-generator-maven-plugin-systemtests/src/test/projects/testproject/src/main/java/some/pck/model/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Some model classes.
- *
- * @author  marmer
- * @date    21.06.2017
- */
-package some.pck.model;
--- java-test-helper-generator-maven-plugin-systemtests/src/test/projects/testproject/src/main/java/some/pck/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Base package.
- *
- * @author  marmer
- * @date    21.06.2017
- */
-package some.pck;
