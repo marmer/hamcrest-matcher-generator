0e908072203726779d96bc72063a3a5ff7742e3a
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2017-06-29 11:08:07 +0200

    dependencies moved into a different package

--- java-test-helper-generator-core/src/test/java/io/github/marmer/testutils/generators/beanmatcher/generation/JavaPoetHasPropertyMatcherClassGeneratorITest.java
+++ java-test-helper-generator-core/src/test/java/io/github/marmer/testutils/generators/beanmatcher/generation/JavaPoetHasPropertyMatcherClassGeneratorITest.java
@@ -1,7 +1,7 @@
 package io.github.marmer.testutils.generators.beanmatcher.generation;
 
+import io.github.marmer.testutils.generators.beanmatcher.dependencies.BasedOn;
 import io.github.marmer.testutils.generators.beanmatcher.generation.JavaPoetHasPropertyMatcherClassGenerator;
-import io.github.marmer.testutils.generators.beanmatcher.generation.dependencies.BasedOn;
 import io.github.marmer.testutils.generators.beanmatcher.processing.BeanPropertyExtractor;
 import io.github.marmer.testutils.generators.beanmatcher.processing.IntrospektorBeanPropertyExtractor;
 import io.github.marmer.testutils.utils.matchers.GeneratedFileCompiler;
