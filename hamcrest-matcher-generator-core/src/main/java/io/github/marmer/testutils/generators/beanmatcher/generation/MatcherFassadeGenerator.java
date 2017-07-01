a43e46cbd4e549ac0ad1362670fd102573c3d04a
MarMer <1055445+marmer@users.noreply.github.com>
2017-07-01 22:39:32 +0200

    unused interface removed

--- hamcrest-matcher-generator-core/src/main/java/io/github/marmer/testutils/generators/beanmatcher/generation/MatcherFassadeGenerator.java
+++
@@ -1,9 +0,0 @@
-package io.github.marmer.testutils.generators.beanmatcher.generation;
-
-/**
- * Generator for Matcher facades.
- *
- * @author  marmer
- * @since    20.06.2017
- */
-public interface MatcherFassadeGenerator { }
