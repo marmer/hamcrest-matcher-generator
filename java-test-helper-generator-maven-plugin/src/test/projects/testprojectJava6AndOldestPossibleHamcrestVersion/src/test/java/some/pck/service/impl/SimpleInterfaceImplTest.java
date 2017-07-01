e75aee99ab3d7bfcd7b5e84f7e3c6920df19db85
MarMer <Mariano.Mertinat@gmail.com>
2017-07-01 12:30:38 +0200

    test fix

--- java-test-helper-generator-maven-plugin/src/test/projects/testprojectJava6AndOldestPossibleHamcrestVersion/src/test/java/some/pck/service/impl/SimpleInterfaceImplTest.java
+++ java-test-helper-generator-maven-plugin/src/test/projects/testprojectJava6AndOldestPossibleHamcrestVersion/src/test/java/some/pck/service/impl/SimpleInterfaceImplTest.java
@@ -1,13 +1,16 @@
 package some.pck.service.impl;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-import static some.pck.model.SimpleModelMatcher.isSimpleModel;
-
 import org.junit.Test;
 
 import some.pck.model.SimpleModel;
 import some.pck.model.SimpleModelMatcher;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import static org.junit.Assert.assertThat;
+
+import static some.pck.model.SimpleModelMatcher.isSimpleModel;
 
 
 public class SimpleInterfaceImplTest {
@@ -15,10 +18,11 @@ public class SimpleInterfaceImplTest {
 	public void testMatchingWithConstructor() throws Exception {
 		assertThat(new SimpleModel("someValue"), is(new SimpleModelMatcher().withSomeProperty(equalTo("someValue"))));
 	}
-	
+
 	@Test
 	public void testMatchingWithStaticMethod() throws Exception {
-		assertThat(new SimpleModel("someValue"), isSimpleModel().withSomeProperty(equalTo("someValue")));
+		assertThat(new SimpleModel("someValue"),
+			isSimpleModel().withSomeProperty(equalTo("someValue")));
 	}
 
 }
