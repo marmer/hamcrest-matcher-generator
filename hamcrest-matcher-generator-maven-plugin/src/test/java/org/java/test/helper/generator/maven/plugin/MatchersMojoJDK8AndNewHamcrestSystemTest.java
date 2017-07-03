a4ccac2bce095ebfbf8d352cedc528b64a20deb4
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2017-07-03 12:48:08 +0200

    goal prefix changed to fit to project name

--- hamcrest-matcher-generator-maven-plugin/src/test/java/org/java/test/helper/generator/maven/plugin/MatchersMojoJDK8AndNewHamcrestSystemTest.java
+++ hamcrest-matcher-generator-maven-plugin/src/test/java/org/java/test/helper/generator/maven/plugin/MatchersMojoJDK8AndNewHamcrestSystemTest.java
@@ -1,99 +1,98 @@
 package org.java.test.helper.generator.maven.plugin;
 
-import org.hamcrest.io.FileMatchers;
-
-import org.java.test.helper.generator.maven.plugin.testutils.TestProjectResource;
-
-import org.junit.Rule;
-import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import static org.hamcrest.Matchers.is;
 
+import org.hamcrest.io.FileMatchers;
 import static org.hamcrest.io.FileMatchers.anExistingFile;
+
+import org.java.test.helper.generator.maven.plugin.testutils.TestProjectResource;
 
 import static org.junit.Assert.assertThat;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 
 public class MatchersMojoJDK8AndNewHamcrestSystemTest {
-	@Rule
-	public TestProjectResource testproject = new TestProjectResource(
-			"testprojectJava8AndCurrentHamcrestVersion");
+    @Rule
+    public TestProjectResource testproject =
+        new TestProjectResource("testprojectJava8AndCurrentHamcrestVersion");
 
-	@Test
-	public void testTestprojectShouldHavePom() throws Exception {
-		assertThat(testproject.pomFile(), FileMatchers.aFileNamed(equalTo("pom.xml")));
-	}
+    @Test
+    public void testTestprojectShouldHavePom() throws Exception {
+        assertThat(testproject.pomFile(), FileMatchers.aFileNamed(equalTo("pom.xml")));
+    }
 
-	@Test
-	public void testTestprojectShouldBeBuildableWithoutMojoExecution() throws Exception {
-		// Preparation
+    @Test
+    public void testTestprojectShouldBeBuildableWithoutMojoExecution() throws Exception {
+        // Preparation
 
-		// Execution
-		final int exitStatus = testproject.executeGoals("clean", "compile");
+        // Execution
+        final int exitStatus = testproject.executeGoals("clean", "compile");
 
-		// Expectation
-		assertThat("Execution exit status", exitStatus, is(0));
-	}
+        // Expectation
+        assertThat("Execution exit status", exitStatus, is(0));
+    }
 
-	@Test
-	public void testPluginExecutionShouldWorkWithoutAnyErrors() throws Exception {
-		// Preparation
+    @Test
+    public void testPluginExecutionShouldWorkWithoutAnyErrors() throws Exception {
+        // Preparation
 
-		// Execution
-		final int exitStatus = testproject.executeGoals("testHelperGenerator:matchers");
+        // Execution
+        final int exitStatus = testproject.executeGoals("hamcrestMatcherGenerator:matchers");
 
-		// Expectation
-		assertThat("Execution exit status", exitStatus, is(0));
-	}
+        // Expectation
+        assertThat("Execution exit status", exitStatus, is(0));
+    }
 
-	@Test
-	public void testPhaseTestShouldStillWorkAfterPluginExecutionWithoutAnyErrors() throws Exception {
-		// Preparation
+    @Test
+    public void testPhaseTestShouldStillWorkAfterPluginExecutionWithoutAnyErrors()
+        throws Exception {
+        // Preparation
 
-		// Execution
-		final int exitStatus = testproject.executeGoals("testHelperGenerator:matchers", "test");
+        // Execution
+        final int exitStatus = testproject.executeGoals("hamcrestMatcherGenerator:matchers",
+                "test");
 
-		// Expectation
-		assertThat("Execution exit status", exitStatus, is(0));
-	}
+        // Expectation
+        assertThat("Execution exit status", exitStatus, is(0));
+    }
 
-	@Test
-	public void testPluginRunHasCreatedMatcherSourceOnCallingPluginGoalDirectly() throws Exception {
+    @Test
+    public void testPluginRunHasCreatedMatcherSourceOnCallingPluginGoalDirectly() throws Exception {
+        // Preparation
+        assertThat("For this test required file is missing",
+            testproject.srcMainJava("some/pck/model/SimpleModel.java"),
+            is(anExistingFile()));
 
-		// Preparation
-		assertThat("For this test required file is missing",
-			testproject.srcMainJava("some/pck/model/SimpleModel.java"),
-			is(anExistingFile()));
+        // Execution
+        testproject.executeGoals("hamcrestMatcherGenerator:matchers");
 
-		// Execution
-		testproject.executeGoals("testHelperGenerator:matchers");
+        // Expectation
+        assertThat(
+            "Should have been generated: " +
+            testproject.generatedTestSourcesDir("some/pck/model/SimpleModelMatcher.java"),
+            testproject.generatedTestSourcesDir("some/pck/model/SimpleModelMatcher.java"),
+            is(anExistingFile()));
+    }
 
-		// Expectation
-		assertThat(
-			"Should have been generated: " +
-			testproject.generatedTestSourcesDir("some/pck/model/SimpleModelMatcher.java"),
-			testproject.generatedTestSourcesDir("some/pck/model/SimpleModelMatcher.java"),
-			is(anExistingFile()));
-	}
+    @Test
+    public void testPluginRunHasCreatedMatcherSourceOnTestGoal() throws Exception {
+        // Preparation
+        assertThat("For this test required file is missing",
+            testproject.srcMainJava("some/pck/model/SimpleModel.java"),
+            is(anExistingFile()));
 
-	@Test
-	public void testPluginRunHasCreatedMatcherSourceOnTestGoal() throws Exception {
+        // Execution
+        testproject.executeGoals("test");
 
-		// Preparation
-		assertThat("For this test required file is missing",
-			testproject.srcMainJava("some/pck/model/SimpleModel.java"),
-			is(anExistingFile()));
-
-		// Execution
-		testproject.executeGoals("test");
-
-		// Expectation
-		assertThat(
-			"Should have been generated: " +
-			testproject.generatedTestSourcesDir("some/pck/model/SimpleModelMatcher.java"),
-			testproject.generatedTestSourcesDir("some/pck/model/SimpleModelMatcher.java"),
-			is(anExistingFile()));
-	}
+        // Expectation
+        assertThat(
+            "Should have been generated: " +
+            testproject.generatedTestSourcesDir("some/pck/model/SimpleModelMatcher.java"),
+            testproject.generatedTestSourcesDir("some/pck/model/SimpleModelMatcher.java"),
+            is(anExistingFile()));
+    }
 }
