d8d00a8e1c894fc9859105ef4d098771042ff6ee
MarMer <1055445+marmer@users.noreply.github.com>
2017-07-03 12:48:08 +0200

    goal prefix changed to fit to project name

--- hamcrest-matcher-generator-maven-plugin/src/test/java/org/java/test/helper/generator/maven/plugin/MatchersMojoNoMatcherSourcesGivenSystemTest.java
+++ hamcrest-matcher-generator-maven-plugin/src/test/java/org/java/test/helper/generator/maven/plugin/MatchersMojoNoMatcherSourcesGivenSystemTest.java
@@ -1,29 +1,28 @@
 package org.java.test.helper.generator.maven.plugin;
 
+import static org.hamcrest.Matchers.is;
+
 import org.java.test.helper.generator.maven.plugin.testutils.TestProjectResource;
+
+import static org.junit.Assert.assertThat;
 
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
-
-import static org.junit.Assert.assertThat;
-
 
 public class MatchersMojoNoMatcherSourcesGivenSystemTest {
-	@Rule
-	public TestProjectResource testproject = new TestProjectResource(
-			"testprojectWithoutMatcherSource");
+    @Rule
+    public TestProjectResource testproject =
+        new TestProjectResource("testprojectWithoutMatcherSource");
 
-	@Test
-	public void testPluginExecutionShouldWorkWithoutAnyErrors() throws Exception {
-		// Preparation
+    @Test
+    public void testPluginExecutionShouldWorkWithoutAnyErrors() throws Exception {
+        // Preparation
 
-		// Execution
-		final int exitStatus = testproject.executeGoals("testHelperGenerator:matchers");
+        // Execution
+        final int exitStatus = testproject.executeGoals("hamcrestMatcherGenerator:matchers");
 
-		// Expectation
-		assertThat("Execution exit status", exitStatus, is(1));
-	}
-
+        // Expectation
+        assertThat("Execution exit status", exitStatus, is(1));
+    }
 }
