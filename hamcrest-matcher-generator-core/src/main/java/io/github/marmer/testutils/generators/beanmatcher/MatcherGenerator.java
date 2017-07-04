e1c3b30fab6836a20fcba4b94f360dfe807e1365
MarMer <1055445+marmer@users.noreply.github.com>
2017-07-04 17:33:22 +0200

    obsolete todos removed

--- hamcrest-matcher-generator-core/src/main/java/io/github/marmer/testutils/generators/beanmatcher/MatcherGenerator.java
+++ hamcrest-matcher-generator-core/src/main/java/io/github/marmer/testutils/generators/beanmatcher/MatcherGenerator.java
@@ -6,18 +6,17 @@ import java.io.IOException;
 /**
  * Generator for Matchers.
  *
- * @author  marmer
- * @since   20.06.2017
+ * @author marmer
+ * @since  20.06.2017
  */
 public interface MatcherGenerator {
-
-	/**
-	 * Generate helper for all classes under the given Package or the qualified class name given.
-	 *
-	 * @param   packageOrQualifiedClassNames  the package or qualified class names
-	 *
-	 * @throws  IOException  Signals that an I/O exception has occurred. TODO should have option to
-	 *                       be robust on errors.
-	 */
-	void generateHelperForClassesAllIn(String... packageOrQualifiedClassNames) throws IOException;
+    /**
+     * Generate helper for all classes under the given Package or the qualified
+     * class name given.
+     *
+     * @param  packageOrQualifiedClassNames the package or qualified class names
+     *
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    void generateHelperForClassesAllIn(String... packageOrQualifiedClassNames) throws IOException;
 }
