1a6419e49de12da7e27a66f479e3a9f11dcb8a38
MarMer <1055445+marmer@users.noreply.github.com>
2017-07-04 15:31:48 +0200

    a little refactoring

--- hamcrest-matcher-generator-dependencies/src/main/java/io/github/marmer/testutils/generators/beanmatcher/dependencies/BeanPropertyMatcher.java
+++ hamcrest-matcher-generator-dependencies/src/main/java/io/github/marmer/testutils/generators/beanmatcher/dependencies/BeanPropertyMatcher.java
@@ -3,6 +3,9 @@ package io.github.marmer.testutils.generators.beanmatcher.dependencies;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
+
+import static org.hamcrest.Matchers.allOf;
+
 import org.hamcrest.TypeSafeMatcher;
 
 import java.util.ArrayList;
@@ -12,42 +15,47 @@ import java.util.List;
 /**
  * Matcher for beans.
  *
- * @param   <T>  Type of the bean.
+ * @param  <T> Type of the bean.
  *
- * @author  marmer
- * @since   13.06.2017
+ * @author marmer
+ * @since  13.06.2017
  */
 public class BeanPropertyMatcher<T> extends TypeSafeMatcher<T> {
-	private final List<Matcher<?>> hasPropertyMatcher = new ArrayList<>();
+    private final List<Matcher<?>> hasPropertyMatcher = new ArrayList<>();
+    private Matcher<?> instanceOfMatcher;
 
-	public BeanPropertyMatcher(final Class<? extends T> expectedClass) {
-		hasPropertyMatcher.add(Matchers.instanceOf(expectedClass));
-	}
+    public BeanPropertyMatcher(final Class<? extends T> expectedClass) {
+        this.instanceOfMatcher = Matchers.instanceOf(expectedClass);
+    }
 
-	@Override
-	public void describeTo(final Description description) {
-		getFullInnerMatcher().describeTo(description);
-	}
+    @Override
+    public void describeTo(final Description description) {
+        getFullInnerMatcher().describeTo(description);
+    }
 
-	@Override
-	protected boolean matchesSafely(final T item) {
-		return getFullInnerMatcher().matches(item);
-	}
+    @Override
+    protected boolean matchesSafely(final T item) {
+        return getFullInnerMatcher().matches(item);
+    }
 
-	@SuppressWarnings("unchecked")
-	private Matcher<?> getFullInnerMatcher() {
-		return Matchers.allOf(hasPropertyMatcher.toArray(new Matcher[hasPropertyMatcher.size()]));
-	}
+    @SuppressWarnings("unchecked")
+    private Matcher<?> getFullInnerMatcher() {
+        final List<Matcher<?>> fullMatcher = new ArrayList<>();
+        fullMatcher.add(instanceOfMatcher);
+        fullMatcher.addAll(hasPropertyMatcher);
 
-	public BeanPropertyMatcher<T> with(final String propertyName, final Matcher<?> matcher) {
-		this.hasPropertyMatcher.add(Matchers.hasProperty(propertyName, matcher));
+        return allOf(fullMatcher.toArray(new Matcher[fullMatcher.size()]));
+    }
 
-		return this;
-	}
+    public BeanPropertyMatcher<T> with(final String propertyName, final Matcher<?> matcher) {
+        this.hasPropertyMatcher.add(Matchers.hasProperty(propertyName, matcher));
 
-	public BeanPropertyMatcher<T> with(final String propertyName) {
-		this.hasPropertyMatcher.add(Matchers.hasProperty(propertyName));
+        return this;
+    }
 
-		return this;
-	}
+    public BeanPropertyMatcher<T> with(final String propertyName) {
+        this.hasPropertyMatcher.add(Matchers.hasProperty(propertyName));
+
+        return this;
+    }
 }
