d4e7b62da34ecc4bbc75e2559b6090efb7853155
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2017-07-20 13:58:23 +0200

    release of 1.2.2

--- hamcrest-matcher-generator-core/src/main/java/io/github/marmer/testutils/generators/beanmatcher/JavaInternalIllegalClassFilter.java
+++ hamcrest-matcher-generator-core/src/main/java/io/github/marmer/testutils/generators/beanmatcher/JavaInternalIllegalClassFilter.java
@@ -4,6 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
+/**
+ * class to filter java internals as illecal classes out of a list.
+ *
+ * @author mertinat
+ * @since  20.07.2017
+ */
 public class JavaInternalIllegalClassFilter implements IllegalClassFilter {
     @Override
     public List<Class<?>> filter(final List<Class<?>> baseClassList) {
