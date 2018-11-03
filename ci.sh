80522052aeaf31d8cf0767bf1ea1131f2a60dba9
MarMer <1055445+marmer@users.noreply.github.com>
2018-11-03 13:37:15 +0100

    final variables removed

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ echo --- Building ---
 echo
 mvn clean install -DskipTests -Dmaven.javadoc.skip -Dmaven.source.skip -Dassembly.skipAssembly -B -V -U
 
-export JAVA_TOOL_OPTIONS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1044"
+#export JAVA_TOOL_OPTIONS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1044"
 echo
 echo --- Testing ---
 echo
