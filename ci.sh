18c80e1234a8891f44865c44cefd9250891a4d9e
marmer <Mariano.Mertinat@gmail.com>
2018-11-03 11:39:32 +0100

    docker compose and script added as reminder

---
+++ ci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo
+echo --- Building ---
+echo
+mvn clean install -DskipTests -Dmaven.javadoc.skip -Dmaven.source.skip -Dassembly.skipAssembly -B -V -U
+
+echo
+echo --- Testing ---
+echo
+mvn clean verify -V
+
