93e772cb40737345db8c9fd0e0a9ae6c4306657d
MarMer <1055445+marmer@users.noreply.github.com>
2018-11-04 21:03:21 +0100

    factory for strategies implemented version pulled up because of expected incompatibilities with older usages of the library

--- hamcrest-matcher-generator-maven-plugin/src/main/java/io/github/marmer/testutils/generators/beanmatcher/mavenplugin/MatcherNamingStrategyFactory.java
+++ hamcrest-matcher-generator-maven-plugin/src/main/java/io/github/marmer/testutils/generators/beanmatcher/mavenplugin/MatcherNamingStrategyFactory.java
@@ -2,18 +2,26 @@ package io.github.marmer.testutils.generators.beanmatcher.mavenplugin;
 
 import io.github.marmer.testutils.generators.beanmatcher.MatcherGeneratorFactory;
 import io.github.marmer.testutils.generators.beanmatcher.generation.MatcherNamingStrategy;
+import io.github.marmer.testutils.generators.beanmatcher.generation.PackageMatcherNamingStrategy;
+import io.github.marmer.testutils.generators.beanmatcher.generation.ParentMatcherNamingStrategy;
 import io.github.marmer.testutils.generators.beanmatcher.generation.PlainMatcherNamingStrategy;
 
 class MatcherNamingStrategyFactory {
     MatcherNamingStrategy strategyFor(final MatcherGeneratorFactory.MatcherGeneratorConfiguration matcherGeneratorConfiguration) {
-        switch (matcherGeneratorConfiguration.getNamingStrategy()) {
-            case PACKAGE:
-                // TODO: marmer 04.11.2018 comming soon
-            case PARENT:
-                // TODO: marmer 04.11.2018 comming soon
-            case PLAIN:
-            default:
-                return new PlainMatcherNamingStrategy();
+        final MatcherNamingStrategy.Name namingStrategy = namingStrategyFrom(matcherGeneratorConfiguration);
+
+        if (MatcherNamingStrategy.Name.PARENT == namingStrategy) {
+            return new ParentMatcherNamingStrategy();
+        } else if (MatcherNamingStrategy.Name.PLAIN == namingStrategy) {
+            return new PlainMatcherNamingStrategy();
+        } else {
+            return new PackageMatcherNamingStrategy();
         }
+    }
+
+    private MatcherNamingStrategy.Name namingStrategyFrom(final MatcherGeneratorFactory.MatcherGeneratorConfiguration matcherGeneratorConfiguration) {
+        return matcherGeneratorConfiguration == null
+                ? null
+                : matcherGeneratorConfiguration.getNamingStrategy();
     }
 }
