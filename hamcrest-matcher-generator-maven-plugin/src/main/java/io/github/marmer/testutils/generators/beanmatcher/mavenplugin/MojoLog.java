6454b41c1e1d3caef1f77e18e051aa1632b8b9da
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2018-11-08 17:18:29 +0100

    equals and hashcode added for tests

--- hamcrest-matcher-generator-maven-plugin/src/main/java/io/github/marmer/testutils/generators/beanmatcher/mavenplugin/MojoLog.java
+++ hamcrest-matcher-generator-maven-plugin/src/main/java/io/github/marmer/testutils/generators/beanmatcher/mavenplugin/MojoLog.java
@@ -1,7 +1,9 @@
 package io.github.marmer.testutils.generators.beanmatcher.mavenplugin;
 
+import lombok.EqualsAndHashCode;
 import org.apache.maven.plugin.logging.Log;
 
+@EqualsAndHashCode
 public class MojoLog implements io.github.marmer.testutils.generators.beanmatcher.Log {
     private final Log log;
 
