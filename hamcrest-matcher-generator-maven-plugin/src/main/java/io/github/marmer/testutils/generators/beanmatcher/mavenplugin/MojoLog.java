68ae3d398d2ddd26757b81110d7b9f9e6e0320d0
MarMer <1055445+marmer@users.noreply.github.com>
2018-11-08 17:31:41 +0100

    back to the value annotation

--- hamcrest-matcher-generator-maven-plugin/src/main/java/io/github/marmer/testutils/generators/beanmatcher/mavenplugin/MojoLog.java
+++ hamcrest-matcher-generator-maven-plugin/src/main/java/io/github/marmer/testutils/generators/beanmatcher/mavenplugin/MojoLog.java
@@ -1,9 +1,9 @@
 package io.github.marmer.testutils.generators.beanmatcher.mavenplugin;
 
-import lombok.EqualsAndHashCode;
+import lombok.Value;
 import org.apache.maven.plugin.logging.Log;
 
-@EqualsAndHashCode
+@Value
 public class MojoLog implements io.github.marmer.testutils.generators.beanmatcher.Log {
     private final Log log;
 
