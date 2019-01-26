7b41884edf406455e05558300f29bfc804d0d3bf
marmer <mariano.mertinat@gmail.com>
2019-01-26 20:52:34 +0100

    Revert "spike for file creation"
    This reverts commit 21ef045c

--- hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/MatcherGenerationProcessor.java
+++ hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/MatcherGenerationProcessor.java
@@ -6,11 +6,6 @@ import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
-import javax.tools.FileObject;
-import javax.tools.JavaFileManager;
-import javax.tools.StandardLocation;
-import java.io.IOException;
-import java.io.Writer;
 import java.util.Set;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
@@ -23,16 +18,6 @@ public class MatcherGenerationProcessor extends AbstractProcessor {
         processingEnv.getElementUtils().getPackageElement("org.hamcrest")
                 .getEnclosedElements()
                 .forEach(o -> {
-                    final JavaFileManager.Location location = StandardLocation.SOURCE_OUTPUT;
-                    try {
-                        final FileObject resource = processingEnv.getFiler().createResource(location, processingEnv.getElementUtils().getPackageOf(o).getQualifiedName().toString(), o.getSimpleName() + ".txt", o);
-                        try (final Writer writer = resource.openWriter()) {
-                            writer.write("bla " + System.currentTimeMillis());
-                        }
-
-                    } catch (final IOException e) {
-                        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Error on filecreation: " + e.getMessage(), o);
-                    }
                     processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, o.toString());
                 });
 
