5a05292c5b1432056777803c5bb73c68ccc929d4
marmer <mariano.mertinat@gmail.com>
2019-01-26 20:52:15 +0100

    Revert "spike"
    This reverts commit 4686bce2

--- hamcrest-matcher-generator-dependencies/src/test/java/io/github/marmer/annotationprocessing/samples/SimplePojo.java
+++
@@ -1,4 +0,0 @@
-package io.github.marmer.annotationprocessing.samples;
-
-public class SimplePojo {
-}
--- hamcrest-matcher-generator-dependencies/src/test/java/io/github/marmer/annotationprocessing/samples3/SamplePojo3.java
+++
@@ -1,4 +0,0 @@
-package io.github.marmer.annotationprocessing.samples3;
-
-public class SamplePojo3 {
-}
