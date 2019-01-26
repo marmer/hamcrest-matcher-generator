18e55ec6916c655aca80bc3097ddf3d96c6828d4
MarMer <1055445+marmer@users.noreply.github.com>
2019-01-26 21:43:46 +0100

    spike

---
+++ hamcrest-matcher-generator-annotation-processor-tests/src/test/java/io/github/marmer/annotationprocessingtest/ProcessingConfiguration.java
@@ -0,0 +1,7 @@
+package io.github.marmer.annotationprocessingtest;
+
+import io.github.marmer.annotationprocessing.MatcherConfiguration;
+
+@MatcherConfiguration("io.github.marmer.annotationprocessingtest.sample.SamplePojo")
+public class ProcessingConfiguration {
+}
