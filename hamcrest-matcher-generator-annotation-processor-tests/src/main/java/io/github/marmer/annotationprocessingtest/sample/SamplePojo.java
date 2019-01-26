2015c4cadf4923dbafcf28d48af94af300c34f49
MarMer <1055445+marmer@users.noreply.github.com>
2019-01-26 23:11:01 +0100

    test spike

--- hamcrest-matcher-generator-annotation-processor-tests/src/main/java/io/github/marmer/annotationprocessingtest/sample/SamplePojo.java
+++ hamcrest-matcher-generator-annotation-processor-tests/src/main/java/io/github/marmer/annotationprocessingtest/sample/SamplePojo.java
@@ -1,7 +1,12 @@
 package io.github.marmer.annotationprocessingtest.sample;
 
+import lombok.Getter;
+
 public class SamplePojo {
     private String someStringProperty;
+
+    @Getter
+    private int anotherProperty;
 
     public String getSomeStringProperty() {
         return someStringProperty;
---
+++ hamcrest-matcher-generator-annotation-processor/src/test/java/io/github/marmer/annotationprocessing/MatcherGenerationProcessorTest.java
@@ -0,0 +1,50 @@
+package io.github.marmer.annotationprocessing;
+
+import com.google.common.truth.Truth;
+import com.google.testing.compile.JavaFileObjects;
+import com.google.testing.compile.JavaSourcesSubjectFactory;
+import com.sun.tools.javac.util.List;
+import org.junit.Test;
+
+import javax.tools.JavaFileObject;
+
+public class MatcherGenerationProcessorTest {
+    @Test
+    public void testSpikeTest()
+            throws Exception {
+        // Preparation
+        final JavaFileObject configuration = JavaFileObjects.forSourceLines("some.pck.SomeConfiguration", "package some.pck;\n" +
+                "\n" +
+                "import io.github.marmer.annotationprocessing.MatcherConfiguration;\n" +
+                "\n" +
+                "@MatcherConfiguration(\"de.bla.SampleClass\")\n" +
+                "public final class SomeConfiguration{\n" +
+                "    \n" +
+                "}");
+        final JavaFileObject javaFileObject = JavaFileObjects.forSourceLines("de.bla.SampleClass", "package de.bla;\n" +
+                "\n" +
+                "public class SampleClass {\n" +
+                "    public String someProperty;\n" +
+                "\n" +
+                "    public String getSomeProperty() {\n" +
+                "        return someProperty;\n" +
+                "    }\n" +
+                "\n" +
+                "    public void setSomeProperty(String someProperty) {\n" +
+                "        this.someProperty = someProperty;\n" +
+                "    }\n" +
+                "}");
+
+        // Execution
+        Truth.assert_()
+                .about(JavaSourcesSubjectFactory.javaSources())
+                .that(List.of(configuration, javaFileObject))
+                .processedWith(new MatcherGenerationProcessor())
+                .compilesWithoutError()
+//                .and()
+//                .generatesSources(OUTPUT_FILES)
+        ;
+
+        // Assertion
+    }
+}
