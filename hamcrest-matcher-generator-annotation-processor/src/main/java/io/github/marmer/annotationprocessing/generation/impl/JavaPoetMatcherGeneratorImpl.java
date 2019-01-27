f8bee3ceaef5a20749c3474568162d322de839cb
MarMer <1055445+marmer@users.noreply.github.com>
2019-01-27 18:57:13 +0100

    some preparation

---
+++ hamcrest-matcher-generator-annotation-processor/src/main/java/io/github/marmer/annotationprocessing/generation/impl/JavaPoetMatcherGeneratorImpl.java
@@ -0,0 +1,12 @@
+package io.github.marmer.annotationprocessing.generation.impl;
+
+import io.github.marmer.annotationprocessing.generation.MatcherGenerator;
+
+public class JavaPoetMatcherGeneratorImpl implements MatcherGenerator {
+    @Override
+    public String generateMatcherFor(final PojoMatcherDescriptor descriptor) {
+        // TODO: marmer 27.01.2019 implement} me
+        return null;
+
+    }
+}
