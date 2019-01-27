3b79550b4c052559f5e3c20af4b0c30dfd915d00
MarMer <1055445+marmer@users.noreply.github.com>
2019-01-27 16:02:26 +0100

    some preparation

---
+++ hamcrest-matcher-generator-annotation-processor/src/main/java/io/github/marmer/annotationprocessing/generation/MatcherGenerator.java
@@ -0,0 +1,28 @@
+package io.github.marmer.annotationprocessing.generation;
+
+import lombok.Builder;
+import lombok.Value;
+import lombok.experimental.Wither;
+
+import java.util.Set;
+
+public interface MatcherGenerator {
+    String generateMatcherFor(PojoMatcherDescriptor descroptor);
+
+    @Value
+    @Builder
+    @Wither
+    class PojoMatcherDescriptor {
+        String typeName;
+        String packageName;
+        Set<PropertyDescriptor> properties;
+    }
+
+    @Value
+    @Builder
+    @Wither
+    class PropertyDescriptor {
+        String typeName;
+        String typePackage;
+    }
+}
