35fc324630370d3f84d46c413a7df439e9ce1ac0
marmer <Mariano.Mertinat@gmail.com>
2019-01-31 20:47:15 +0100

    First steps test preparation

--- hamcrest-matcher-generator-annotationprocessor/src/test/java/io/github/marmer/annotationprocessing/MatcherGenerationProcessorIT.java
+++ hamcrest-matcher-generator-annotationprocessor/src/test/java/io/github/marmer/annotationprocessing/MatcherGenerationProcessorIT.java
@@ -1,51 +1,51 @@
 package io.github.marmer.annotationprocessing;
 
 import com.google.common.truth.Truth;
+import com.google.testing.compile.CompileTester;
 import com.google.testing.compile.JavaFileObjects;
 import com.google.testing.compile.JavaSourcesSubjectFactory;
-import com.sun.tools.javac.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import javax.tools.JavaFileObject;
 
-public class MatcherGenerationProcessorIT {
+import static java.util.Arrays.asList;
+
+class MatcherGenerationProcessorIT {
+
     @Test
-    public void testSpikeTest()
+    @DisplayName("Matcher should have been generated for Pojo from Source file")
+    void testGenerate_MatcherShouldHaveBeenGeneratedForPojoFromSourceFile()
             throws Exception {
         // Preparation
         final JavaFileObject configuration = JavaFileObjects.forSourceLines("some.pck.SomeConfiguration", "package some.pck;\n" +
                 "\n" +
                 "import io.github.marmer.annotationprocessing.MatcherConfiguration;\n" +
+                "import some.other.pck;\n" +
                 "\n" +
-                "@MatcherConfiguration(\"some.pck.SimplePojo\")\n" +
+                "@MatcherConfiguration(SimplePojo.class)\n" +
                 "public final class SomeConfiguration{\n" +
                 "    \n" +
                 "}");
-        final JavaFileObject javaFileObject = JavaFileObjects.forSourceLines("de.bla.SampleClass", "package some.pck;\n" +
-                "\n" +
-                "public class SimplePojo {\n" +
-                "    public String someProperty;\n" +
-                "\n" +
-                "    public String getSomeProperty() {\n" +
-                "        return someProperty;\n" +
-                "    }\n" +
-                "}");
 
-        final JavaFileObject expectedOutput = JavaFileObjects.forSourceString("sample.output.OutputClass", "package some.pck;\n" +
+        final JavaFileObject javaFileObject = JavaFileObjects.forSourceLines("some.other.pck.PojoWithoutProperties", "package some.other.pck;\n" +
                 "\n" +
-                "import javax.annotation.Generated;\n" +
-                "\n" +
-                "@Generated({\"By hamcrest matcher generator: https://github.com/marmer/hamcrest-matcher-generator\"})\n" +
                 "public class SimplePojoMatcher{\n" +
                 "}");
 
+        final JavaFileObject expectedOutput = JavaFileObjects.forSourceString("sample.other.pck.OutputClass", "package some.other.pck;\n" +
+                "\n" +
+                "public class SimplePojo{\n" +
+                "}");
+
         // Execution
-        // Assertion
-        Truth.assert_()
+        final CompileTester compileTester = Truth.assert_()
                 .about(JavaSourcesSubjectFactory.javaSources())
-                .that(List.of(configuration, javaFileObject))
-                .processedWith(new MatcherGenerationProcessor())
-                .compilesWithoutError()
+                .that(asList(configuration, javaFileObject))
+                .processedWith(new MatcherGenerationProcessor());
+
+        // Assertion
+        compileTester.compilesWithoutError()
                 .and()
                 .generatesSources(expectedOutput)
         ;
