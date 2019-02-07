313f9b1b954c1afff1d99c85e396a3eb12bfc5f4
marmer <mariano.mertinat@gmail.com>
2019-02-07 23:25:39 +0100

    Simple roundtrip without JavaPoet yet

--- hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/creation/SourceWriter.java
+++ hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/creation/SourceWriter.java
@@ -2,11 +2,10 @@ package io.github.marmer.annotationprocessing.creation;
 
 import io.github.marmer.annotationprocessing.core.model.MatcherSourceDescriptor;
 
-import java.io.IOException;
-import java.io.Writer;
-
 import javax.annotation.processing.Filer;
 import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.Writer;
 
 public class SourceWriter {
     public void create(final Filer filer, final MatcherSourceDescriptor matcherSourceDescriptor) {
@@ -14,13 +13,10 @@ public class SourceWriter {
 
         // TODO: marmer 31.01.2019 implement me. This is just some dummy code
         try {
-            final JavaFileObject sourceFile = filer.createSourceFile("some.other.pck.SimplePojoMatcher");
+            final JavaFileObject sourceFile = filer.createSourceFile(matcherSourceDescriptor.getType().getPackageName() + "." + matcherSourceDescriptor.getType().getTypeName());
             try (final Writer writer = sourceFile
                     .openWriter()) {
-                writer.write("package some.other.pck;\n" +
-                        "\n" +
-                        "public class SimplePojoMatcher{\n" +
-                        "}");
+                writer.write(matcherSourceDescriptor.getSource());
                 writer.flush();
             }
         } catch (final IOException e) {
