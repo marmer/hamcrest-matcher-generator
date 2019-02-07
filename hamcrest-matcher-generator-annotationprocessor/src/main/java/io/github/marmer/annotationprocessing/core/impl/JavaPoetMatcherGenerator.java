6d7cfa4c4ff587560faed44242703963794b2e8b
MarMer <1055445+marmer@users.noreply.github.com>
2019-02-07 23:25:39 +0100

    Simple roundtrip without JavaPoet yet

--- hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/core/impl/JavaPoetMatcherGenerator.java
+++ hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/core/impl/JavaPoetMatcherGenerator.java
@@ -3,12 +3,18 @@ package io.github.marmer.annotationprocessing.core.impl;
 import io.github.marmer.annotationprocessing.core.MatcherGenerator;
 import io.github.marmer.annotationprocessing.core.model.MatcherBaseDescriptor;
 import io.github.marmer.annotationprocessing.core.model.MatcherSourceDescriptor;
+import io.github.marmer.annotationprocessing.core.model.TypeDescriptor;
 
 public class JavaPoetMatcherGenerator implements MatcherGenerator {
     @Override
     public MatcherSourceDescriptor generateMatcherFor(final MatcherBaseDescriptor descriptor) {
-
-        // TODO: marmer 31.01.2019 implement me
-        return MatcherSourceDescriptor.builder().build();
+        return MatcherSourceDescriptor.builder()
+                .type(TypeDescriptor.builder()
+                        .packageName(descriptor.getBase().getPackageName())
+                        .typeName(descriptor.getBase().getTypeName() + "Matcher").build())
+                .source("package some.other.pck;\n" +
+                        "\n" +
+                        "public class SimplePojoMatcher{\n" +
+                        "}").build();
     }
 }
--- hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/extraction/MatcherBaseDescriptorfFactory.java
+++ hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/extraction/MatcherBaseDescriptorfFactory.java
@@ -1,20 +1,16 @@
 package io.github.marmer.annotationprocessing.extraction;
 
-import com.google.auto.common.AnnotationValues;
-import com.google.auto.common.MoreElements;
-import com.google.auto.common.MoreTypes;
 import io.github.marmer.annotationprocessing.MatcherConfiguration;
 import io.github.marmer.annotationprocessing.MatcherConfigurations;
 import io.github.marmer.annotationprocessing.core.model.MatcherBaseDescriptor;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import io.github.marmer.annotationprocessing.core.model.TypeDescriptor;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.TypeElement;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Factory to create some matcher descriptions.
@@ -43,14 +39,19 @@ public class MatcherBaseDescriptorfFactory {
      * @return Resulting {@link MatcherBaseDescriptor}s based on the configurations.
      */
     public Set<MatcherBaseDescriptor> create(final MatcherConfiguration configurations, final ProcessingEnvironment processingEnv) {
-        final String[] value = configurations.value();
-        final Set<MatcherBaseDescriptor> result = new HashSet<>();
-        final TypeElement type = processingEnv.getElementUtils().getTypeElement(value[0]);
+
         // TODO: marmer 01.02.2019 Type does not exist -> warn
-        AnnotationValues.
+        return Stream.of(configurations.value())
+                .map(t -> processingEnv.getElementUtils().getTypeElement(t))
+                .map(typeElement -> typeDescriptorFor(processingEnv, typeElement))
+                .collect(Collectors.toSet());
+    }
 
-
-        return result;
+    private MatcherBaseDescriptor typeDescriptorFor(final ProcessingEnvironment processingEnv, final TypeElement type) {
+        return MatcherBaseDescriptor.builder()
+                .base(TypeDescriptor.builder()
+                        .packageName(processingEnv.getElementUtils().getPackageOf(type).getQualifiedName().toString())
+                        .typeName(type.getSimpleName().toString()).build()).build();
     }
 
 
