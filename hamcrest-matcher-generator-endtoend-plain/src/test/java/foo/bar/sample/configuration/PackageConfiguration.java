d61256df45059d370ff5171781500700b0af2a99
MarMer <1055445+marmer@users.noreply.github.com>
2019-02-18 21:00:03 +0100

    additional test for inheritance matchers

--- hamcrest-matcher-generator-endtoend-plain/src/test/java/foo/bar/sample/configuration/PackageConfiguration.java
+++ hamcrest-matcher-generator-endtoend-plain/src/test/java/foo/bar/sample/configuration/PackageConfiguration.java
@@ -4,6 +4,6 @@ package foo.bar.sample.configuration;
 import io.github.marmer.annotationprocessing.MatcherConfiguration;
 import io.github.marmer.annotationprocessing.MatcherConfigurations;
 
-@MatcherConfigurations(@MatcherConfiguration("foo.bar.sample.model.SomePojo"))
+@MatcherConfigurations(@MatcherConfiguration({"foo.bar.sample.model.SomePojo", "foo.bar.sample.model.ParentPojo"}))
 public class PackageConfiguration {
 }
--- hamcrest-matcher-generator-endtoend-plain/src/test/java/foo/bar/sample/model/SomePojoTest.java
+++ hamcrest-matcher-generator-endtoend-plain/src/test/java/foo/bar/sample/model/SomePojoTest.java
@@ -4,6 +4,7 @@ import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static foo.bar.sample.model.ParentPojoMatcher.isParentPojo;
 import static foo.bar.sample.model.SomePojoMatcher.isSomePojo;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -18,9 +19,6 @@ class SomePojoTest {
         somePojo.setPojoField("pojoFieldValue");
         somePojo.setParentField("someParentFieldValue");
 
-        // Execution
-
-
         // Assertion
         MatcherAssert.assertThat(somePojo, isSomePojo()
                 .withClass(SomePojo.class)
@@ -28,6 +26,23 @@ class SomePojoTest {
                 .withParentField(is(equalTo("someParentFieldValue")))
                 .withPojoField("pojoFieldValue")
                 .withPojoField(is(equalTo("pojoFieldValue")))
+        );
+    }
+
+    @Test
+    @DisplayName("Matchers of parent classes should work fine as well")
+    void testMatchers_MatchersOfParentClassesShouldWorkFineAsWell()
+            throws Exception {
+        // Preparation
+        final SomePojo somePojo = new SomePojo();
+        somePojo.setPojoField("pojoFieldValue");
+        somePojo.setParentField("someParentFieldValue");
+
+        // Assertion
+        MatcherAssert.assertThat(somePojo, isParentPojo()
+                .withClass(SomePojo.class)
+                .withParentField("someParentFieldValue")
+                .withParentField(is(equalTo("someParentFieldValue")))
         );
     }
 }
