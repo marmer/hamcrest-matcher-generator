d798aee26ea756e2c2c2002bd44389b4735cf8e2
MarMer <1055445+marmer@users.noreply.github.com>
2019-02-20 15:28:04 +0100

    - end to end test for interfaces

--- hamcrest-matcher-generator-endtoend-plain/src/test/java/foo/bar/sample/configuration/PackageConfiguration.java
+++ hamcrest-matcher-generator-endtoend-plain/src/test/java/foo/bar/sample/configuration/PackageConfiguration.java
@@ -4,6 +4,11 @@ package foo.bar.sample.configuration;
 import io.github.marmer.annotationprocessing.MatcherConfiguration;
 import io.github.marmer.annotationprocessing.MatcherConfigurations;
 
-@MatcherConfigurations(@MatcherConfiguration({"foo.bar.sample.model.SomePojo", "foo.bar.sample.model.ParentPojo"}))
+@MatcherConfigurations(
+        @MatcherConfiguration({
+                "foo.bar.sample.model.SomePojo",
+                "foo.bar.sample.model.ParentPojo",
+                "foo.bar.sample.model.SomePojoInterface",
+        }))
 public class PackageConfiguration {
 }
