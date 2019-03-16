b5254fec6c3f2cfb6d149fb1670c26f86cb83198
MarMer <1055445+marmer@users.noreply.github.com>
2019-03-16 10:40:11 +0100

    a little more logging

--- hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/MessagerLogger.java
+++ hamcrest-matcher-generator-annotationprocessor/src/main/java/io/github/marmer/annotationprocessing/MessagerLogger.java
@@ -3,12 +3,13 @@ package io.github.marmer.annotationprocessing;
 import io.github.marmer.annotationprocessing.core.Logger;
 
 import javax.annotation.processing.Messager;
+import javax.lang.model.element.Element;
 import javax.tools.Diagnostic;
 
 public class MessagerLogger implements Logger {
     private final Messager messager;
 
-    public MessagerLogger(final Messager messager) {
+    MessagerLogger(final Messager messager) {
         this.messager = messager;
     }
 
@@ -21,5 +22,15 @@ public class MessagerLogger implements Logger {
     public void error(final String message) {
         messager.printMessage(Diagnostic.Kind.MANDATORY_WARNING, message);
 
+    }
+
+    @Override
+    public void info(final String message, final Element element) {
+        messager.printMessage(Diagnostic.Kind.NOTE, message, element);
+    }
+
+    @Override
+    public void info(final String message) {
+        messager.printMessage(Diagnostic.Kind.NOTE, message);
     }
 }
