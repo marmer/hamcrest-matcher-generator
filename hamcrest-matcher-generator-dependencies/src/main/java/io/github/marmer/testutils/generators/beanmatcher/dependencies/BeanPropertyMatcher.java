af3323b912160c8a2abcf43aa2c88470d82e30c7
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-06-18 17:50:04 +0200

    intermediate state

--- hamcrest-matcher-generator-dependencies/src/main/java/io/github/marmer/testutils/generators/beanmatcher/dependencies/BeanPropertyMatcher.java
+++ hamcrest-matcher-generator-dependencies/src/main/java/io/github/marmer/testutils/generators/beanmatcher/dependencies/BeanPropertyMatcher.java
@@ -1,83 +1,81 @@
 package io.github.marmer.testutils.generators.beanmatcher.dependencies;
 
+import static org.hamcrest.Matchers.allOf;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.Matchers.allOf;
-
 
 /**
  * Matcher for beans.
  *
- * @param   <T>  Type of the bean.
- *
- * @author  marmer
- * @since   13.06.2017
+ * @param <T> Type of the bean.
+ * @author marmer
+ * @since 13.06.2017
  */
 public class BeanPropertyMatcher<T> extends TypeSafeMatcher<T> {
-	@SuppressWarnings("squid:S2293")
-	private final List<Matcher<?>> hasPropertyMatcher = new ArrayList<Matcher<?>>();
-	private Matcher<?> instanceOfMatcher;
 
-	public BeanPropertyMatcher(final Class<? extends T> expectedClass) {
-		this.instanceOfMatcher = Matchers.instanceOf(expectedClass);
-	}
+    @SuppressWarnings("squid:S2293")
+    private final List<Matcher<?>> hasPropertyMatcher = new ArrayList<>();
+    private final Matcher<?> instanceOfMatcher;
 
-	@Override
-	public void describeTo(final Description description) {
-		getFullInnerMatcher().describeTo(description);
-	}
+    public BeanPropertyMatcher(final Class<? super T> expectedClass) {
+        instanceOfMatcher = Matchers.instanceOf(expectedClass);
+    }
 
-	@Override
-	protected boolean matchesSafely(final T item) {
-		return getFullInnerMatcher().matches(item);
-	}
+    @Override
+    public void describeTo(final Description description) {
+        getFullInnerMatcher().describeTo(description);
+    }
 
-	@SuppressWarnings("unchecked")
-	private Matcher<?> getFullInnerMatcher() {
-		@SuppressWarnings("squid:S2293")
-		final List<Matcher<?>> fullMatcher = new ArrayList<Matcher<?>>();
-		fullMatcher.add(instanceOfMatcher);
-		fullMatcher.addAll(hasPropertyMatcher);
+    @Override
+    protected boolean matchesSafely(final T item) {
+        return getFullInnerMatcher().matches(item);
+    }
 
-		return allOf(fullMatcher.toArray(new Matcher[fullMatcher.size()]));
-	}
+    @SuppressWarnings("unchecked")
+    private Matcher<?> getFullInnerMatcher() {
+        @SuppressWarnings("squid:S2293") final List<Matcher<?>> fullMatcher = new ArrayList<>();
+        fullMatcher.add(instanceOfMatcher);
+        fullMatcher.addAll(hasPropertyMatcher);
 
-	public BeanPropertyMatcher<T> with(final String propertyName, final Matcher<?> matcher) {
-		this.hasPropertyMatcher.add(Matchers.hasProperty(propertyName, matcher));
+        return allOf(fullMatcher.toArray(new Matcher[fullMatcher.size()]));
+    }
 
-		return this;
-	}
+    public BeanPropertyMatcher<T> with(final String propertyName, final Matcher<?> matcher) {
+        hasPropertyMatcher.add(Matchers.hasProperty(propertyName, matcher));
 
-	public BeanPropertyMatcher<T> with(final String propertyName) {
-		this.hasPropertyMatcher.add(Matchers.hasProperty(propertyName));
+        return this;
+    }
 
-		return this;
-	}
+    public BeanPropertyMatcher<T> with(final String propertyName) {
+        hasPropertyMatcher.add(Matchers.hasProperty(propertyName));
 
-	@Override
-	protected void describeMismatchSafely(final T item, final Description mismatchDescription) {
-		boolean missmatchDescriptionAllreadyAdded = false;
+        return this;
+    }
 
-		if (!instanceOfMatcher.matches(item)) {
-			mismatchDescription.appendText("Is an instance of " + item.getClass());
-			missmatchDescriptionAllreadyAdded = true;
-		}
+    @Override
+    protected void describeMismatchSafely(final T item, final Description mismatchDescription) {
+        boolean missmatchDescriptionAllreadyAdded = false;
 
-		for (final Matcher<?> matcher : hasPropertyMatcher) {
-			if (!matcher.matches(item)) {
-				if (missmatchDescriptionAllreadyAdded) {
-					mismatchDescription.appendText(" and ");
-				}
+        if (!instanceOfMatcher.matches(item)) {
+            mismatchDescription.appendText("Is an instance of " + item.getClass());
+            missmatchDescriptionAllreadyAdded = true;
+        }
 
-				matcher.describeMismatch(item, mismatchDescription);
-				missmatchDescriptionAllreadyAdded = true;
-			}
-		}
-	}
+        for (final Matcher<?> matcher : hasPropertyMatcher) {
+            if (!matcher.matches(item)) {
+                if (missmatchDescriptionAllreadyAdded) {
+                    mismatchDescription.appendText(" and ");
+                }
+
+                matcher.describeMismatch(item, mismatchDescription);
+                missmatchDescriptionAllreadyAdded = true;
+            }
+        }
+    }
 }
